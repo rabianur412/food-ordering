f094b1a1e97d8256aa54b03a223e81c7efd1d427
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-02 15:12:30 +0300

    Click outside, useState(), props

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,7 +1,11 @@
-import Logo from "../ui/Logo"
+import { useState } from "react"
 import {FaUserAlt,FaSearch,FaShoppingCart} from "react-icons/fa"
+import Logo from "../ui/Logo"
+import OutsideClickHandler from "react-outside-click-handler"
+import Title from "../ui/Title"
 
 const Header = () => {
+  const [isSearchModal, setIsSearchModal] = useState(false)
   return (
     <div className="h-[5.5rem] bg-secondary "> 
       <div className="container mx-auto text-white flex justify-between items-center h-full">
@@ -24,19 +28,27 @@ const Header = () => {
         </nav>
         <div className="flex gap-x-4 items-center">
           <a href="#">
-            <FaUserAlt/>         
+            <FaUserAlt className="hover:text-primary cursor-pointer"/>         
           </a>
           <a href="#">
-          <FaShoppingCart/>
+          <FaShoppingCart className="hover:text-primary cursor-pointer"/>
           </a>
-          <a href="#">
-          <FaSearch/>
-          </a>
+          <button onClick={()=>setIsSearchModal(true)}>
+          <FaSearch className="hover:text-primary cursor-pointer"/>
+          </button>
           <a href="#">
           <button className="btn-primary">Siparişlerim</button>
           </a>
         </div>
       </div>
+      {isSearchModal && (
+        <OutsideClickHandler onOutsideClick={()=>setIsSearchModal(false)}>
+         <div >
+          <Title addClass="text-9xl">Title</Title>
+
+         </div>
+      </OutsideClickHandler>
+      )}
     </div>
   )
 }
