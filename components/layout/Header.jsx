91fd0b4b9d07b0ab0c7752988b243f5b51b3b857
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-03 21:55:43 +0300

    header refactoring

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react"
 import {FaUserAlt,FaSearch,FaShoppingCart} from "react-icons/fa"
+import{GiHamburgerMenu} from "react-icons/gi"
+import{AiFillCloseCircle} from "react-icons/ai"
 import Logo from "../ui/Logo"
 import Search from "../ui/Search"
 
 
+
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false)
+  const[isMenuModal,setIsMenuModal] =useState(false);
   return (
     <div className="h-[5.5rem] bg-secondary "> 
       <div className="container mx-auto text-white flex justify-between items-center h-full">
             <Logo/>
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav 
+        className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full
+         sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden
+         ${isMenuModal === true && "!grid place-content-center" }`}>
+          <ul className="flex gap-x-2 sm:flex-row flex-col items-center ">
             <li className="px-[5px] py-[12px] uppercase hover:text-primary cursor-pointer">
               <a href="">Ana Sayfa</a>
             </li>
@@ -25,20 +32,29 @@ const Header = () => {
               <a href="">Book Table</a>
             </li>
           </ul>
+          {isMenuModal&&(
+            <button className="absolute top-4 right-4 z-50 " onClick={()=>setIsMenuModal(false)}>
+            <AiFillCloseCircle size={30} className="text-black transition-all hover:text-primary"/>
+         </button>
+          )}
         </nav>
         <div className="flex gap-x-4 items-center">
           <a href="#">
             <FaUserAlt className="hover:text-primary cursor-pointer"/>         
           </a>
           <a href="#">
-          <FaShoppingCart className="hover:text-primary cursor-pointer"/>
+            <FaShoppingCart className="hover:text-primary cursor-pointer"/>
           </a>
           <button onClick={()=>setIsSearchModal(true)}>
-          <FaSearch className="hover:text-primary cursor-pointer"/>
+            <FaSearch className="hover:text-primary cursor-pointer"/>
           </button>
-          <a href="#">
-          <button className="btn-primary">Siparişlerim</button>
+          <a href="#" className="md:inline-block hidden sm">
+             <button className="btn-primary">Siparişlerim</button>
           </a>
+          <button className="sm:hidden inline-block" onClick={()=>setIsMenuModal(true)}>
+            <GiHamburgerMenu className="text-xl hover:text-primary transition-all" />
+          </button>
+          
         </div>
       </div>
       {isSearchModal && (
