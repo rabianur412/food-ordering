46528d658cea7814878b855ad930a977014362c7
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-12 21:38:36 +0300

    Input component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Home from "./home"
 import Header from "../components/layout/Header"
+import Input from '@/components/form/Input'
 
 export default function Index() {
   return (
@@ -15,6 +16,9 @@ export default function Index() {
       </Head>
       <Header/>
      <Home/>
+     <div className="p-20 container mx-auto">
+     <Input/>
+     </div>
      
     </div>
     
